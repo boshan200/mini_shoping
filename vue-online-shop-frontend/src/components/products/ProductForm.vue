19daf5c0571a7059219757135d01854155836f1a
boshan200 <boshan200@gmail.com>
2020-08-24 21:24:42 +0800

    first commit without publish

---
+++ vue-online-shop-frontend/src/components/products/ProductForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="productInfo">
+    <el-form class="form" ref="form" :model="model" label-width="180px">
+      <el-form-item label="Name">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item label="Price">
+        <el-input v-model="model.price"></el-input>
+      </el-form-item>
+      <el-form-item label="Manufacturer ">
+        <el-select v-model="model.manufacturer.name" clearable placeholder="请选择制造商">
+          <el-option
+            v-for="manufacturer in manufacturers"
+            :key="manufacturer._id"
+            :label="manufacturer.name"
+            :value="manufacturer.name">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Image ">
+        <el-input v-model="model.image"></el-input>
+      </el-form-item>
+      <el-form-item label="Description ">
+        <el-input type="textarea" v-model="model.description"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button v-if="isEditing" type="primary" @click="onSubmit">Update Product</el-button>
+        <el-button v-else @click="onSubmit">Add Product</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['model', 'manufacturers', 'isEditing'],
+  created() {
+    console.log(this.model)
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('save-product', this.model)
+    }
+  }
+}
+</script>
+<style>
+.productInfo {
+  padding-top: 10px;
+}
+.form {
+  margin: 0 auto;
+  width: 500px;
+}
+.el-input__inner {
+  height: 60px;
+}
+</style>
---
+++ vue-online-shop-frontend/src/components/products/ProductList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="products">
+      <div class="container">
+        This is ProductList
+      </div>
+        <product-item :products="products"></product-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue';
+export default {
+  name: 'product-list',
+  created(){
+    if (this.products.length === 0) {
+      this.$store.dispatch('allProducts')
+    }
+  },
+  computed: {
+    // a computed getter
+    products() {
+      return this.$store.getters.allProducts;
+    }
+  },
+  components: {
+    'product-item': ProductItem
+  }
+}
+</script>
+
